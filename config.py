a66ebd3010aaa7b1a1bc3606ac2d9b04ba51844a
Cann <69559214+yeshareal@users.noreply.github.com>
2021-02-25 09:45:27 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+token = 'ODE0MTUyMDYxNjA3ODcwNTI1.YDZsMQ.NtCYwD0EwrsAi2zysYttFfP9JJE' # your bot's token
+client_id = 814152061607870525 # your bot's client id
+owner_id = 357768011593023499 # 
+discordbotlist_key = '' # discordbotlist.com api key
+emojis = {
+    'name': '<:name:739452243262046240>',
+    'limit': '<:limit:739452243895517254>',
+    'category': '<:category:739452243949912176>',
+    'position': '<:position:739452243861962962>',
+    'bitrate': '<:bitrate:771374579695616031>',
+    'help': '<:help:739471623702315029>',
+    'exit': '<:exit:739452244298301501>'
+}
