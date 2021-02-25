61a18a5064337e3b8a5f340c5167cbf11600dba4
Cann <69559214+yeshareal@users.noreply.github.com>
2021-02-25 13:14:33 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, menus
 import config
 from cogs.help import HelpCommand
 from utils.jsonfile import JSONList, JSONDict
